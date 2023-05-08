6fbea1261c46e18ccebb00d1b3ef1fd702478d63
isLinXu <2267379130@qq.com>
2023-05-09 01:47:49 +0800

    [hf_downloader] update modeling_map.py

---
+++ hf_downloader/modeling_map.py
@@ -0,0 +1,156 @@
+
+__init__modeling_map = [
+    "ALBERT_PRETRAINED_CONFIG_ARCHIVE_MAP",
+    "BART_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "BERT_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "CAMEMBERT_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "DISTILBERT_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "ELECTRA_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "FLAUBERT_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "GPT2_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "PRETRAINED_MODEL_ARCHIVE_MAP",
+    "OPENAI_GPT_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "ROBERTA_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "T5_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "TF_ALBERT_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "ALBERT_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "BART_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "BERT_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "CAMEMBERT_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "CTRL_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "DISTILBERT_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "ELECTRA_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "FLAUBERT_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "GPT2_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "PRETRAINED_MODEL_ARCHIVE_MAP",
+    "OPENAI_GPT_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "ROBERTA_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "T5_PRETRAINED_MODEL_ARCHIVE_MAP",
+    "TF_ALBERT_PRETRAINED_MODEL_ARCHIVE_MAP"
+]
+
+
+
+ALBERT_PRETRAINED_MODEL_ARCHIVE_MAP = {
+    "albert-base-v1": "https://cdn.huggingface.co/albert-base-v1-pytorch_model.bin",
+    "albert-large-v1": "https://cdn.huggingface.co/albert-large-v1-pytorch_model.bin",
+    "albert-xlarge-v1": "https://cdn.huggingface.co/albert-xlarge-v1-pytorch_model.bin",
+    "albert-xxlarge-v1": "https://cdn.huggingface.co/albert-xxlarge-v1-pytorch_model.bin",
+    "albert-base-v2": "https://cdn.huggingface.co/albert-base-v2-pytorch_model.bin",
+    "albert-large-v2": "https://cdn.huggingface.co/albert-large-v2-pytorch_model.bin",
+    "albert-xlarge-v2": "https://cdn.huggingface.co/albert-xlarge-v2-pytorch_model.bin",
+    "albert-xxlarge-v2": "https://cdn.huggingface.co/albert-xxlarge-v2-pytorch_model.bin",
+}
+
+
+BART_PRETRAINED_MODEL_ARCHIVE_MAP = {
+    "bart-large": "https://cdn.huggingface.co/facebook/bart-large/pytorch_model.bin",
+    "bart-large-mnli": "https://cdn.huggingface.co/facebook/bart-large-mnli/pytorch_model.bin",
+    "bart-large-cnn": "https://cdn.huggingface.co/facebook/bart-large-cnn/pytorch_model.bin",
+    "bart-large-xsum": "https://cdn.huggingface.co/facebook/bart-large-xsum/pytorch_model.bin",
+    "mbart-large-en-ro": "https://cdn.huggingface.co/facebook/mbart-large-en-ro/pytorch_model.bin",
+}
+
+BERT_PRETRAINED_MODEL_ARCHIVE_MAP = {
+    "bert-base-uncased": "https://cdn.huggingface.co/bert-base-uncased-pytorch_model.bin",
+    "bert-large-uncased": "https://cdn.huggingface.co/bert-large-uncased-pytorch_model.bin",
+    "bert-base-cased": "https://cdn.huggingface.co/bert-base-cased-pytorch_model.bin",
+    "bert-large-cased": "https://cdn.huggingface.co/bert-large-cased-pytorch_model.bin",
+    "bert-base-multilingual-uncased": "https://cdn.huggingface.co/bert-base-multilingual-uncased-pytorch_model.bin",
+    "bert-base-multilingual-cased": "https://cdn.huggingface.co/bert-base-multilingual-cased-pytorch_model.bin",
+    "bert-base-chinese": "https://cdn.huggingface.co/bert-base-chinese-pytorch_model.bin",
+    "bert-base-german-cased": "https://cdn.huggingface.co/bert-base-german-cased-pytorch_model.bin",
+    "bert-large-uncased-whole-word-masking": "https://cdn.huggingface.co/bert-large-uncased-whole-word-masking-pytorch_model.bin",
+    "bert-large-cased-whole-word-masking": "https://cdn.huggingface.co/bert-large-cased-whole-word-masking-pytorch_model.bin",
+    "bert-large-uncased-whole-word-masking-finetuned-squad": "https://cdn.huggingface.co/bert-large-uncased-whole-word-masking-finetuned-squad-pytorch_model.bin",
+    "bert-large-cased-whole-word-masking-finetuned-squad": "https://cdn.huggingface.co/bert-large-cased-whole-word-masking-finetuned-squad-pytorch_model.bin",
+    "bert-base-cased-finetuned-mrpc": "https://cdn.huggingface.co/bert-base-cased-finetuned-mrpc-pytorch_model.bin",
+    "bert-base-german-dbmdz-cased": "https://cdn.huggingface.co/bert-base-german-dbmdz-cased-pytorch_model.bin",
+    "bert-base-german-dbmdz-uncased": "https://cdn.huggingface.co/bert-base-german-dbmdz-uncased-pytorch_model.bin",
+    "bert-base-japanese": "https://cdn.huggingface.co/cl-tohoku/bert-base-japanese/pytorch_model.bin",
+    "bert-base-japanese-whole-word-masking": "https://cdn.huggingface.co/cl-tohoku/bert-base-japanese-whole-word-masking/pytorch_model.bin",
+    "bert-base-japanese-char": "https://cdn.huggingface.co/cl-tohoku/bert-base-japanese-char/pytorch_model.bin",
+    "bert-base-japanese-char-whole-word-masking": "https://cdn.huggingface.co/cl-tohoku/bert-base-japanese-char-whole-word-masking/pytorch_model.bin",
+    "bert-base-finnish-cased-v1": "https://cdn.huggingface.co/TurkuNLP/bert-base-finnish-cased-v1/pytorch_model.bin",
+    "bert-base-finnish-uncased-v1": "https://cdn.huggingface.co/TurkuNLP/bert-base-finnish-uncased-v1/pytorch_model.bin",
+    "bert-base-dutch-cased": "https://cdn.huggingface.co/wietsedv/bert-base-dutch-cased/pytorch_model.bin",
+}
+
+CAMEMBERT_PRETRAINED_MODEL_ARCHIVE_MAP = {
+    "camembert-base": "https://cdn.huggingface.co/camembert-base-pytorch_model.bin",
+    "umberto-commoncrawl-cased-v1": "https://cdn.huggingface.co/Musixmatch/umberto-commoncrawl-cased-v1/pytorch_model.bin",
+    "umberto-wikipedia-uncased-v1": "https://cdn.huggingface.co/Musixmatch/umberto-wikipedia-uncased-v1/pytorch_model.bin",
+}
+
+CTRL_PRETRAINED_MODEL_ARCHIVE_MAP = {"ctrl": "https://storage.googleapis.com/sf-ctrl/pytorch/seqlen256_v1.bin"}
+
+DISTILBERT_PRETRAINED_MODEL_ARCHIVE_MAP = {
+    "distilbert-base-uncased": "https://cdn.huggingface.co/distilbert-base-uncased-pytorch_model.bin",
+    "distilbert-base-uncased-distilled-squad": "https://cdn.huggingface.co/distilbert-base-uncased-distilled-squad-pytorch_model.bin",
+    "distilbert-base-cased": "https://cdn.huggingface.co/distilbert-base-cased-pytorch_model.bin",
+    "distilbert-base-cased-distilled-squad": "https://cdn.huggingface.co/distilbert-base-cased-distilled-squad-pytorch_model.bin",
+    "distilbert-base-german-cased": "https://cdn.huggingface.co/distilbert-base-german-cased-pytorch_model.bin",
+    "distilbert-base-multilingual-cased": "https://cdn.huggingface.co/distilbert-base-multilingual-cased-pytorch_model.bin",
+    "distilbert-base-uncased-finetuned-sst-2-english": "https://cdn.huggingface.co/distilbert-base-uncased-finetuned-sst-2-english-pytorch_model.bin",
+}
+
+ELECTRA_PRETRAINED_MODEL_ARCHIVE_MAP = {
+    "google/electra-small-generator": "https://cdn.huggingface.co/google/electra-small-generator/pytorch_model.bin",
+    "google/electra-base-generator": "https://cdn.huggingface.co/google/electra-base-generator/pytorch_model.bin",
+    "google/electra-large-generator": "https://cdn.huggingface.co/google/electra-large-generator/pytorch_model.bin",
+    "google/electra-small-discriminator": "https://cdn.huggingface.co/google/electra-small-discriminator/pytorch_model.bin",
+    "google/electra-base-discriminator": "https://cdn.huggingface.co/google/electra-base-discriminator/pytorch_model.bin",
+    "google/electra-large-discriminator": "https://cdn.huggingface.co/google/electra-large-discriminator/pytorch_model.bin",
+}
+
+FLAUBERT_PRETRAINED_MODEL_ARCHIVE_MAP = {
+    "flaubert-small-cased": "https://cdn.huggingface.co/flaubert/flaubert_small_cased/pytorch_model.bin",
+    "flaubert-base-uncased": "https://cdn.huggingface.co/flaubert/flaubert_base_uncased/pytorch_model.bin",
+    "flaubert-base-cased": "https://cdn.huggingface.co/flaubert/flaubert_base_cased/pytorch_model.bin",
+    "flaubert-large-cased": "https://cdn.huggingface.co/flaubert/flaubert_large_cased/pytorch_model.bin",
+}
+
+
+
+GPT2_PRETRAINED_MODEL_ARCHIVE_MAP = {
+    "gpt2": "https://cdn.huggingface.co/gpt2-pytorch_model.bin",
+    "gpt2-medium": "https://cdn.huggingface.co/gpt2-medium-pytorch_model.bin",
+    "gpt2-large": "https://cdn.huggingface.co/gpt2-large-pytorch_model.bin",
+    "gpt2-xl": "https://cdn.huggingface.co/gpt2-xl-pytorch_model.bin",
+    "distilgpt2": "https://cdn.huggingface.co/distilgpt2-pytorch_model.bin",
+}
+
+
+PRETRAINED_MODEL_ARCHIVE_MAP = {
+    "opus-mt-en-de": "https://cdn.huggingface.co/Helsinki-NLP/opus-mt-en-de/pytorch_model.bin",
+}
+
+OPENAI_GPT_PRETRAINED_MODEL_ARCHIVE_MAP = {"openai-gpt": "https://cdn.huggingface.co/openai-gpt-pytorch_model.bin"}
+
+ROBERTA_PRETRAINED_MODEL_ARCHIVE_MAP = {
+    "roberta-base": "https://cdn.huggingface.co/roberta-base-pytorch_model.bin",
+    "roberta-large": "https://cdn.huggingface.co/roberta-large-pytorch_model.bin",
+    "roberta-large-mnli": "https://cdn.huggingface.co/roberta-large-mnli-pytorch_model.bin",
+    "distilroberta-base": "https://cdn.huggingface.co/distilroberta-base-pytorch_model.bin",
+    "roberta-base-openai-detector": "https://cdn.huggingface.co/roberta-base-openai-detector-pytorch_model.bin",
+    "roberta-large-openai-detector": "https://cdn.huggingface.co/roberta-large-openai-detector-pytorch_model.bin",
+}
+
+T5_PRETRAINED_MODEL_ARCHIVE_MAP = {
+    "t5-small": "https://cdn.huggingface.co/t5-small-pytorch_model.bin",
+    "t5-base": "https://cdn.huggingface.co/t5-base-pytorch_model.bin",
+    "t5-large": "https://cdn.huggingface.co/t5-large-pytorch_model.bin",
+    "t5-3b": "https://cdn.huggingface.co/t5-3b-pytorch_model.bin",
+    "t5-11b": "https://cdn.huggingface.co/t5-11b-pytorch_model.bin",
+}
+
+TF_ALBERT_PRETRAINED_MODEL_ARCHIVE_MAP = {
+    "albert-base-v1": "https://cdn.huggingface.co/albert-base-v1-with-prefix-tf_model.h5",
+    "albert-large-v1": "https://cdn.huggingface.co/albert-large-v1-with-prefix-tf_model.h5",
+    "albert-xlarge-v1": "https://cdn.huggingface.co/albert-xlarge-v1-with-prefix-tf_model.h5",
+    "albert-xxlarge-v1": "https://cdn.huggingface.co/albert-xxlarge-v1-with-prefix-tf_model.h5",
+    "albert-base-v2": "https://cdn.huggingface.co/albert-base-v2-with-prefix-tf_model.h5",
+    "albert-large-v2": "https://cdn.huggingface.co/albert-large-v2-with-prefix-tf_model.h5",
+    "albert-xlarge-v2": "https://cdn.huggingface.co/albert-xlarge-v2-with-prefix-tf_model.h5",
+    "albert-xxlarge-v2": "https://cdn.huggingface.co/albert-xxlarge-v2-with-prefix-tf_model.h5",
+}
