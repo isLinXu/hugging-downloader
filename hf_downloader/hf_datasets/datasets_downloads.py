f5e7ff7bd0559c0e35e94ba476165ff2373abba5
isLinXu <2267379130@qq.com>
2023-07-06 12:23:56 +0800

    [hf_downloader] update datasets_downloads.py

--- hf_downloader/hf_datasets/datasets_downloads.py
+++ hf_downloader/hf_datasets/datasets_downloads.py
@@ -1,11 +1,23 @@
+import os
+import sys
 
-from pprint import pprint
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
 import json
-import hf_downloader.profiles
-from datasets import load_dataset,list_datasets
+# import hf_downloader.profiles
+from datasets import load_dataset, list_datasets
 
-datasets_list = list_datasets()
-print("datasets_list:",datasets_list)
-with open("../../download/datasets_all.json", "w") as json_file:
-    json.dump(datasets_list, json_file)
 
+def get_hf_datasets_info(save_path="../../download/datasets_all.json", debug=True):
+    datasets_list = list_datasets()
+    print("datasets_list:", datasets_list)
+    if os.path.exists(save_path) == False:
+        os.makedirs(save_path)
+    with open(save_path, "w") as json_file:
+        json.dump(datasets_list, json_file)
+
+
+if __name__ == '__main__':
+    save_path = "/home/linxu/PycharmProjects/hugging-downloader/download/datasets_all.json"
+    get_hf_datasets_info(save_path)
